cedc70f44937bbcef169d5c85a4d24fde2490857
Christian Rainier Patiño Lara <isc.christianlara@gmail.com>
2024-08-24 17:30:55 -0600

    Add archive page

---
+++ app/archive/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Archive</h1>
+      <Link href="/">Home</Link>
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>The future is near...</h1>
+      <Link href="/archive">Archive</Link>
     </main>
   );
 }
